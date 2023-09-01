79795fe85c801d5dd9866fc271b96b173d8d3c53
Forrest Jablonski <highoffscience@gmail.com>
2023-08-31 20:22:12 -0400

    Setting up...

---
+++ README.md
@@ -0,0 +1,5 @@
+Implements the flame algorithm <https://flam3.com/flame_draves.pdf>
+
+In progress...
+
+This is a birthday program
---
+++ flame.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file    flame.h
+ * @version 1.0.0
+ * @author  Forrest Jablonski
+ */
+
+int main(void)
+{
+   return 0;
+}
